4c0042613e33d231f0e16b2ce513aac1a8b4617a
syahruddin <syahruddinr@gmail.com>
2021-11-03 18:48:33 +0800

    fix maxlength

--- gate_frontend.php
+++ gate_frontend.php
@@ -15,11 +15,11 @@
       <input type="text" id="username" name="username" required>
       <br><br>
       <label for="owner">CARD NUMBER</label><br>
-      <input type="number" id="cnumber" name="cnumber" maxlength="19" required><br>
+      <input oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" type="number" id="cnumber" name="cnumber" maxlength="19" required><br>
       <label for="exp">EXPIRATION DATE</label><br>
       <input type="month" id="exp" name="exp" required><br><br>
       <label for="cvc">CV CODE</label><br>
-      <input type="number" id="cvc" name="cvc" minlength="3" maxlength="3" required><br><br>
+      <input oninput="javascript: if (this.value.length > this.maxLength) this.value = this.value.slice(0, this.maxLength);" type="number" id="cvc" name="cvc" maxlength="3" required><br><br>
       <label for="owner">CARD OWNER</label><br>
       <input type="text" id="owner" name="owner" required><br><br>
       <input type="submit" value="Submit">
